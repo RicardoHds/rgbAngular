262d8099aa79b52be8ea33a2abccc90374215d99
Adrian Ruvalcaba Garcia <adrian.ruvalcaba@sistemas.tecsanpedro.edu.mx>
2018-07-26 12:20:36 -0500

    Se agrego la funcion de cambio de colores por RGB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  color: string;
+  color1:number;
+  color2:number;
+  color3:number;
+  RGB: string;
+  miRGB(){
+
+    this.color1 = this.ruletaColores();
+    this.color2 = this.ruletaColores();
+    this.color3 = this.ruletaColores();
+ 
+    this.RGB = ('rgba('+this.color1 +','+this.color2 +','+this.color3 +')');
+    return 'rgba('+this.color1 +','+this.color2 +','+this.color3 +')';
+
+  }
+  ruletaColores(){
+    //Math.floor(Math.random()*255);
+    return Math.floor(Math.random()*255);;
+  }
 }
